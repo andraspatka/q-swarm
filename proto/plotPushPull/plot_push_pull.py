c40c704b6c0c8e5ef361c6ab9a3f57182e5dc597
Patka Zsolt-Andras <andras.patka@gmail.com>
2020-03-21 22:02:11 +0200

    Adjusted gauss curve parameters

--- proto/plotPushPull/plot_push_pull.py
+++ proto/plotPushPull/plot_push_pull.py
@@ -4,20 +4,20 @@ import numpy as np
 x = np.linspace(0, 1, num=100)  # measured distance
 B_PUSH = 0  # The robot's centre
 B_PULL = 1  # The prox sensor's coverage limit
-C_PUSH = 0.15  # width of the gauss curve
+C_PUSH = 0.2  # width of the gauss curve
 C_PULL = 0.2
 A = 1  # height of the gauss curve
 
 num_push = - (x - B_PUSH) ** 2
 denum_push = 2 * C_PUSH ** 2
 y_push = A * np.exp(num_push / denum_push)
-y_push[y_push < 0.1] = 0
+y_push[y_push <= 0.15] = 0
 push_plot = plt.plot(x, y_push, label="Push")
 
 num_pull = - (x - B_PULL) ** 2
 denum_pull = 2 * C_PULL ** 2
 y_pull = A * np.exp(num_pull / denum_pull)
-y_pull[y_pull < 0.1] = 0
+y_pull[y_pull <= 0.15] = 0
 pull_plot = plt.plot(x, y_pull, label="Pull")
 
 plt.xlabel("Distance")
