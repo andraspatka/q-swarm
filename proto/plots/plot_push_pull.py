3108552c300a67449d063c542c382239aead1982
Patka Zsolt-Andras <andras.patka@gmail.com>
2020-04-26 23:07:10 +0300

    Updated the push pull forces plot

--- proto/plots/plot_push_pull.py
+++ proto/plots/plot_push_pull.py
@@ -6,29 +6,30 @@ WHITE = 'white'
 BLACK = 'black'
 mpl.rcParams['text.color'] = BLACK
 mpl.rcParams['font.size'] = 16
-mpl.rcParams['axes.labelcolor'] = WHITE
-mpl.rcParams['xtick.color'] = WHITE
-mpl.rcParams['ytick.color'] = WHITE
+mpl.rcParams['axes.labelcolor'] = BLACK
+mpl.rcParams['xtick.color'] = BLACK
+mpl.rcParams['ytick.color'] = BLACK
 mpl.rcParams['lines.linewidth'] = 4
 
 x_plt = np.linspace(0, 3, num=100)
 B_PUSH = 0  # The robot's centre
 B_PULL = 3  # The prox sensor's coverage limit
 C_PUSH = 0.5  # width of the gauss curve
-C_PULL = 0.8
+C_PULL = 0.7
+cut_off = 0.15
 A = 1  # height of the gauss curve
 
 num_push = - (x_plt - B_PUSH) ** 2
 denum_push = 2 * C_PUSH ** 2
 y_push = A * np.exp(num_push / denum_push)
-y_push[y_push <= 0.15] = 0
+y_push[y_push <= cut_off] = 0
 y_push[x_plt > 1] = 0
 push_plot = plt.plot(x_plt, y_push, label="Push")
 
 num_pull = - (x_plt - B_PULL) ** 2
 denum_pull = 2 * C_PULL ** 2
 y_pull = A * np.exp(num_pull / denum_pull)
-y_pull[y_pull <= 0.15] = 0
+y_pull[y_pull <= cut_off] = 0
 pull_plot = plt.plot(x_plt, y_pull, label="Pull")
 plt.xlabel("Distance")
 plt.ylabel("Magnitude")
